b1b0a394a53a247093a6326dad3be0af3684d316
KaterinaKab <katerina.kabanova97@gmail.com>
2020-10-22 01:10:13 +0900

    Nested Loop Added

---
+++ src/com/syntax/class09/PrintingPatternsBestWay.java
@@ -0,0 +1,15 @@
+package com.syntax.class09;
+
+public class PrintingPatternsBestWay {
+
+	public static void main(String[] args) {
+		
+		for (int r=7; r>0; r--) {
+			for (int c=1; c<=6; c++ ) {
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/com/syntax/class09/PruntingPatternsLongWay.java
@@ -0,0 +1,35 @@
+package com.syntax.class09;
+
+public class PruntingPatternsLongWay{
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=5; i++ ) {
+			System.out.print("*");
+		}
+		
+		/*****
+		 *****
+		 *****
+		 *****
+		 */
+		
+		System.out.println();
+		for (int i=1; i<=5; i++ ) {
+			System.out.print("*");
+		}
+		System.out.println();
+		for (int i=1; i<=5; i++ ) {
+			System.out.print("*");
+			
+		}
+		System.out.println();
+		for (int i=1; i<=5; i++ ) {
+			System.out.print("*");
+		}
+		
+		
+		
+	}
+
+}
